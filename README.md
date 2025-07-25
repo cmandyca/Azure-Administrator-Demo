2907f77c295f367050d8a62e5489a434c5787674
cmandyca <cmandy.ca@gmail.com>
2025-07-25 12:09:12 -0700

    Add files via upload
    Initial commit: Add Terraform code for Azure infrastructure

---
+++ README.md
@@ -0,0 +1,108 @@
+````markdown
+# Azure Small Business Infrastructure Demo
+
+This project deploys a foundational, secure, and scalable infrastructure for a hypothetical small business on Microsoft Azure. The entire environment is defined and managed as code using Terraform, demonstrating best practices for automation and resource management in the cloud.
+
+This project is a portfolio piece designed to showcase core skills required for an Azure Administrator role.
+
+## Architecture Diagram
+
+A high-level diagram of the infrastructure deployed by this Terraform code.
+
+*A simple diagram showing the components below should be embedded here.*
+
+![Architecture Diagram](images/architecture.png)
+
+*Diagram components: VNet -> Subnet -> NSG -> VM with a Public IP*
+
+## Skills & Features Demonstrated
+
+This project showcases the following Azure and DevOps competencies:
+
+*   **Infrastructure as Code (IaC):** All resources are defined declaratively using **Terraform**.
+*   **Core Networking:**
+    *   Creation of a **Virtual Network (VNet)** to provide an isolated network environment.
+    *   Segmentation using **Subnets** for organizing resources.
+*   **Network Security:**
+    *   Implementation of a **Network Security Group (NSG)**.
+    *   Configuration of an inbound security rule to allow **RDP access** for administration.
+*   **Compute:**
+    *   Deployment of a **Windows Server Virtual Machine**.
+*   **Public IP & Connectivity:**
+    *   Allocation of a **Standard SKU Public IP address** for external access.
+*   **Resource Management:**
+    *   Logical grouping of all resources within a single **Resource Group**.
+
+## Technologies Used
+
+*   **Microsoft Azure**
+*   **Terraform**
+*   **Azure CLI**
+
+## How to Deploy
+
+### Prerequisites
+
+1.  **Azure CLI:** You must have the Azure CLI installed.
+2.  **Terraform:** You must have Terraform installed.
+3.  **Azure Subscription:** An active Azure subscription (Pay-As-You-Go is sufficient).
+
+### Deployment Steps
+
+1.  **Clone the repository:**
+    ```bash
+    git clone <your-repo-url>
+    cd <your-repo-directory>
+    ```
+
+2.  **Log in to Azure:**
+    ```bash
+    az login
+    ```
+
+3.  **Initialize Terraform:**
+    This command downloads the necessary Azure provider plugin.
+    ```bash
+    terraform init
+    ```
+
+4.  **Plan the deployment:**
+    This command shows you a preview of the resources that will be created.
+    ```bash
+    terraform plan
+    ```
+
+5.  **Apply the configuration:**
+    This command builds the infrastructure. Type `yes` when prompted.
+    ```bash
+    terraform apply
+    ```
+
+## Deployed Resources
+
+After running `terraform apply`, the following key resources will be created in your Azure subscription:
+
+*   A Resource Group named `MyDemoProject-RG`.
+*   A Virtual Network named `MyDemo-VNet`.
+*   A Subnet named `Web-Subnet`.
+*   A Network Security Group named `Web-Subnet-NSG` with a rule to allow RDP.
+*   A Standard Public IP address.
+*   A Network Interface.
+*   A Windows Server Virtual Machine named `Web-VM`.
+
+## Connecting to the VM
+
+1.  After the `terraform apply` command completes, it will output the public IP address of the server.
+2.  Use a Remote Desktop client to connect to this IP address.
+3.  **Username:** `azureadmin`
+4.  **Password:** The password you set in the `main.tf` file.
+
+## Cleanup
+
+To avoid ongoing charges, it is critical to destroy all resources when you are finished.
+
+Run the following command to delete everything created by this project:
+```bash
+terraform destroy
+```
+````
---
+++ main.tf
@@ -0,0 +1,112 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "MyDemoProject-RG"
+  location = "West US 2"
+}
+
+# 2. Create a Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "MyDemo-VNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# 3. Create a Network Security Group (NSG) and an RDP Rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "Web-Subnet-NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+# 4. Create a Subnet for the Web Server
+resource "azurerm_subnet" "web_subnet" {
+  name                 = "Web-Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 5. Associate the NSG with the Subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.web_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# 6. Create a Public IP for the Web Server (CHANGED TO STANDARD SKU)
+resource "azurerm_public_ip" "web_pip" {
+  name                = "Web-PublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard" # <-- THE FIX IS HERE
+}
+
+# 7. Create a Network Interface for the Web Server
+resource "azurerm_network_interface" "web_nic" {
+  name                = "web-server-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web_pip.id
+  }
+}
+
+# 8. Create the Web Server Virtual Machine (Windows)
+resource "azurerm_windows_virtual_machine" "web_vm" {
+  name                = "Web-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" # Free tier eligible
+  admin_username      = "azureadmin"
+  admin_password      = "Password1234!" # IMPORTANT: Change this to a secure password
+  network_interface_ids = [
+    azurerm_network_interface.web_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+# 9. Output the Public IP address of the Web Server
+output "web_server_public_ip" {
+  value = azurerm_public_ip.web_pip.ip_address
+}
